ed744406d911f9b9b01d4197f82aaab8a30a4f20
talimera <talhameral@gmail.com>
2020-05-04 07:25:02 -0700

    Finding Duplicates using Lambda Expression

---
+++ src/JavaClasses/day33_CustomClass/ArrayList_Duplicates.java
@@ -0,0 +1,34 @@
+package JavaClasses.day33_CustomClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.Predicate;
+
+public class ArrayList_Duplicates {
+
+    public static void main(String[] args) {
+
+        /*
+        write a program that can return the unique objects from a anArray List of Integers
+            Ex:
+                list: {1,1,2,3,4,5,5}
+                sout(list); ==> {2,3,4};
+        Note: 1. DO NOT use any extra arrayList
+              2. DO NOT use any loops
+              3. do not use any sort method
+              4. use predicate only and collections methods only
+        Hint:   Collections.frequency() // will return the frequency
+                removeIf( frequency == 1)  can keep the unique objects in arrayList
+
+         */
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,1,2,3,4,5,5));
+
+        Predicate<Integer> a = p -> Collections.frequency(list,p)==1; // removing uniques by finding the frequency of them
+        list.removeIf(a);
+
+        System.out.println(list);
+    }
+}
