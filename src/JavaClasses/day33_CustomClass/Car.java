aa6b255ec16d66014f90ed86e020fcc7f0f35f0b
talimera <talhameral@gmail.com>
2020-05-04 08:54:05 -0700

    Car class

---
+++ src/JavaClasses/day33_CustomClass/Car.java
@@ -0,0 +1,16 @@
+package JavaClasses.day33_CustomClass;
+
+public class Car {
+
+    String Brand; // instance variables
+    String Model; // instance variables
+    int year; // instance variables
+    String color; // instance variables
+
+
+    public static void main(String[] args) {
+
+        int a = 100; // local variable
+    }
+
+}
