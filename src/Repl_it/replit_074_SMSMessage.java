84c0c1b330cfa72a596fa936fce1fa7975a5e916
talimera <talhameral@gmail.com>
2020-05-11 21:56:58 -0700

    Sms Message

---
+++ src/Repl_it/replit_074_SMSMessage.java
@@ -0,0 +1,43 @@
+package Repl_it;
+
+import java.util.Scanner;
+
+public class replit_074_SMSMessage {
+
+    /*
+        We have a message variable already declared and assigned value in this format
+
+        Sender:<Mike Smith>. From Number:[202-123-3456]. Message:{I love programing and problem solving}
+
+        Variables are already declared:
+
+        sender, phoneNumber, messageBody
+        - by using String methods:
+        retrieve related information from SMSmessage string and assign to those 3 variables.
+        -print each variable in separate line
+
+        Sender: Mike Smith
+        Phone Number: 202-123-3456
+        Message body: I love programing and problem solving
+     */
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String message = scan.nextLine();
+            String sender;
+            String phoneNumber;
+            String messageBody;
+
+            sender = message.substring(message.indexOf("<"), message.indexOf(">" + 1));
+
+            phoneNumber = message.substring(message.indexOf("["), message.indexOf("]" + 1));
+
+            messageBody = message.substring(message.indexOf("{"), message.indexOf("}" + 1));
+
+            System.out.println("Sender: " + sender);
+            System.out.println("Phone Number: " + phoneNumber);
+            System.out.println("Message body: " + messageBody);
+
+        }
+
+}
